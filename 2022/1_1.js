8b80c19c5f56499413a647460e749d7f19f4d14b
olivier Ballerini-Gendrin <olivier@lespacedunmatin.info>
2022-12-04 23:51:14 +0100

    Advent of code 2022 day 1

---
+++ 2022/1_1.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+class BandOfElves {
+    elves = []
+
+    get maxCaloriesCarriedByAnElf() {
+        return this.elves.reduce((max, elf) => max > elf.totalCalories ? max : elf.totalCalories, 0)
+    }
+
+    constructor(elves) {
+        this.elves = elves
+    }
+
+    newElf() {
+        this.elves.push(new Elf())
+    }
+}
+class Elf {
+    bag = []
+
+    get totalCalories() {
+        return this.bag.reduce((acc, value) => acc + value, 0)
+    }
+
+    constructor() {
+    }
+}
+
+function treatData(data) {
+
+    const groupDataPerElf = () => {
+        const lines = data.split('\n')
+        const bandOfElves = new BandOfElves([new Elf()])
+
+        for (const line of lines) {
+            if (line === '') {
+                bandOfElves.newElf();
+                continue
+            }
+
+            bandOfElves.elves[bandOfElves.elves.length - 1].bag.push(parseInt(line, 10))
+        }
+
+        return bandOfElves
+    }
+
+    const bandOfElves = groupDataPerElf()
+    console.log(bandOfElves.maxCaloriesCarriedByAnElf) // Expected 69177
+}
+
+fs.readFile('./data/1.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+
+    treatData(data);
+})
